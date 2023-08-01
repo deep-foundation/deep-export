aabfb2261fc4339c8a8c59e0f87b0fe5ecd9e0e3
freephoenix888 <freephoenix888@gmail.com>
2023-08-01 09:01:37 +0000

    Update documentation

---
+++ docs/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = JSON.parse("{\"rows\":[{\"kind\":64,\"name\":\"exportData\",\"url\":\"functions/exportData.html\",\"classes\":\"\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"exportdata\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
